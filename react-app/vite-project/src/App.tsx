c4154cdc5a63406cd48ab1c351b6ffed7517eaf8
Prateek Varshney <prateek.varshney@se.com>
2025-09-11 19:27:08 +0530

    added product type selection

--- react-app/vite-project/src/App.tsx
+++ react-app/vite-project/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import RightPanel from "./RightPanel.tsx";
 
 function App() {
--- react-app/vite-project/src/RightPanel.tsx
+++ react-app/vite-project/src/RightPanel.tsx
@@ -1,29 +1,106 @@
+import { useEffect, useRef, useState } from "react";
+
+const productTypes = [
+  {
+    id: "Necklace",
+    label: "NECKLACE",
+    img: "https://www.astridandmiyu.com/cdn/shop/files/Q1PartB2953_a5059151-0843-45b1-95f7-736df3cce84e.webp?v=1706887315&width=720", // replace with actual image
+  },
+  {
+    id: "Anklet",
+    label: "ANKLET",
+    img: "https://www.astridandmiyu.com/cdn/shop/files/Q1PartB2953_a5059151-0843-45b1-95f7-736df3cce84e.webp?v=1706887315&width=720",
+  },
+  {
+    id: "Bracelet",
+    label: "BRACELET",
+    img: "https://www.astridandmiyu.com/cdn/shop/files/Q1PartB2953_a5059151-0843-45b1-95f7-736df3cce84e.webp?v=1706887315&width=720",
+  },
+];
+
 export default function RightPanel() {
+  const [customProductDetails, setCustomProductDetails] = useState({
+    productType: "",
+    style: "",
+    length: "",
+    charms: [] as string[],
+  });
+
+  const ref = useRef(document.getElementById("third-screen-section-2"));
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        if (
+          mutation.type === "attributes" &&
+          mutation?.attributeName?.startsWith("data-")
+        ) {
+          setCustomProductDetails({
+            ...customProductDetails,
+            productType:
+              (mutation.target as Element).getAttribute(
+                mutation.attributeName
+              ) ?? "",
+          });
+        }
+      }
+    });
+
+    observer.observe(ref.current, {
+      attributes: true,
+      attributeOldValue: true,
+    });
+
+    return () => observer.disconnect();
+  }, [ref]);
+
   return (
-    <div className="right-panel">
-      {/* Product Type */}
-      <div className="panel-section">
-        <h3 className="section-title">PRODUCT TYPE</h3>
-        <button className="expand-btn">›</button>
-      </div>
+    <div className="container">
+      <div className="left-panel">
+        {/* Product Type */}
+        <div className="panel-section">
+          <h3 className="section-title">PRODUCT TYPE</h3>
+          {customProductDetails.productType && customProductDetails.productType}
+          <button className="expand-btn">›</button>
+        </div>
 
-      {/* Style */}
-      <div className="panel-section">
-        <h3 className="section-title">STYLE</h3>
-        <button className="expand-btn">›</button>
-      </div>
+        {/* Style */}
+        <div className="panel-section">
+          <h3 className="section-title">STYLE</h3>
+          <button className="expand-btn">›</button>
+        </div>
 
-      {/* Length */}
-      <div className="panel-section">
-        <h3 className="section-title">LENGTH</h3>
-        <button className="expand-btn">›</button>
-      </div>
+        {/* Length */}
+        <div className="panel-section">
+          <h3 className="section-title">LENGTH</h3>
+          <button className="expand-btn">›</button>
+        </div>
 
-      {/* Charms */}
-      <div className="panel-section">
-        <h3 className="section-title">CHARMS</h3>
-        <span className="charms-count">0/7 SELECTED</span>
-        <button className="expand-btn">›</button>
+        {/* Charms */}
+        <div className="panel-section">
+          <h3 className="section-title">CHARMS</h3>
+          <span className="charms-count">0/7 SELECTED</span>
+          <button className="expand-btn">›</button>
+        </div>
+      </div>
+      <div className="right-panel">
+        <div className="grid-container">
+          {productTypes.map((product) => (
+            <div
+              className={`grid-item ${
+                customProductDetails.productType === product.id
+                  ? "selected"
+                  : ""
+              }`}
+              key={product.id}
+            >
+              <img src={product.img} alt="Necklace" />
+              <div className="label">{product.label}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
